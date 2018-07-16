7a597b1244542f9d19893806663f1c20e0c4f005
ilyana19 <aries_2152@hotmail.com>
2018-07-16 14:59:16 -0400

    added energy component

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,5 @@
 distance = 0 #current distance
+energy = 5
 
 while (true) #loop won't stop unless they go home
   puts "============================"
@@ -12,14 +13,24 @@ while (true) #loop won't stop unless they go home
   case action
   when "walk"
     distance += 1
+    energy += 1
+    puts "Current energy: #{energy}"
     puts "Distance from home is #{distance}km."
   when "run"
-    distance += 5
-    puts "Distance from home is #{distance}km."
+    if (energy > 0)
+      distance += 5
+      energy -= 5
+      puts "Current energy: #{energy}"
+      puts "Distance from home is #{distance}km."
+    else
+      puts "Not enough energy to run".upcase
+      puts "\nCurrent energy: #{energy}"
+      puts "Distance from home is #{distance}km."
+    end
   when "go home"
     puts "You're going home!"
     break #exit the loop & program
   else
-    puts "Invalid Input"
+    puts "Invalid Input".upcase
   end
 end
