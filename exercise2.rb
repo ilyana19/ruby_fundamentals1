10799d85c0c1bd636f307b78cc26a8eb2b42192d
ilyana19 <aries_2152@hotmail.com>
2018-07-16 13:05:50 -0400

    exercise2 solution

---
+++ exercise2.rb
@@ -0,0 +1,16 @@
+# q1 solution
+cost_of_meal = 55
+tips = 0.15
+puts "The tip is: #{cost_of_meal * tips}."
+
+# q2 solution
+text = "hello"
+num = 5
+puts text + num.to_s
+
+# q3 solution
+puts "Answer: #{45628 * 7839}"
+
+# q4 solution
+# true because the or (||) is true so it's "truthy"
+puts (10 < 20 && 30 < 20) || !(10 == 11)
